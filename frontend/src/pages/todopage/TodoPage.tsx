f399e7173e727333c80c2198cf14088835c3eee9
Rinae Hyun <rnhyun.dream@gmail.com>
2024-08-18 12:40:52 +0200

    Install react-router-dom

---
+++ frontend/src/pages/todopage/TodoPage.tsx
@@ -0,0 +1,14 @@
+import Header from "../../components/Header.tsx";
+import TodoGallery from "../../components/TodoGallery.tsx";
+import AddTodo from "../../components/AddTodo.tsx";
+
+export default function TodoPage() {
+
+    return (
+        <>
+            <Header/>
+            <TodoGallery/>
+            <AddTodo/>
+        </>
+    )
+}
